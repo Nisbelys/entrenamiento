a15ba79f4061a19096c8ceb92a89e5ae2dc529f0
Nisbelys <salazarnisbelys@gmail.com>
2022-05-14 00:47:27 +0000

    [ADD] Nuevo modulo de pratica

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Odoo Academy",
+    'description': """Módulo de odoo para el aprendisaje""",
+    'summary': """ """,
+    "category": '',
+    "version": '1.0',
+    'author': 'Nisbelys Salazar',
+    'company': 'CIMAS Proyectos',
+    'maintainer': '',
+    'website': "",
+    "depends": ['base'],
+    "data": [
+        
+    ],
+    
+    "demo": [
+        
+    ],
+    
+    'images': [
+        'static/description/banner.png',
+    ],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': Fa¡lse,
+    'auto_install': False,
+}
---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Odoo Academy",
+    'description': """Módulo de odoo para el aprendisaje""",
+    'summary': """ """,
+    "category": '',
+    "version": '1.0',
+    'author': 'Nisbelys Salazar',
+    'company': 'CIMAS Proyectos',
+    'maintainer': '',
+    'website': "",
+    "depends": ['base'],
+    "data": [
+        
+    ],
+    
+    "demo": [
+        
+    ],
+    
+    'images': [
+        
+    ],
+   
+}
